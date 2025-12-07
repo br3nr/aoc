19a4fa30f5488860c827b13cb14d48a505ae0148
br3nr <repetitive_routine@protonmail.com>
2025-12-07 23:46:41 +1100

    ★★

--- 2025/day_05/star_2.py
+++ 2025/day_05/star_2.py
@@ -1,38 +1,38 @@
-# note: 
-# simple enough but I wonder how my solution will affect part 2?
+# note:
+# first loop checking for overlaps of windows: o(n^2) worst case
+# second loops adds up the window sizs.
+# also the windows are sorted by the first value
 
+# im on a plane to sydney with no wifi so cant test if
+# this works (update: it worked)
 
-f = open("test.txt")
-f = f.read().split("\n\n")
-
-ranges = f[0].split("\n")
-
-c = 0
+f = open("input.txt")
+ranges = f.read().split("\n\n")[0].split("\n")
 
 vals = []
 for r in ranges:
     l, h = map(int, r.split("-"))
-    vals.append([l,h])
+    vals.append([l, h])
 
 vals.sort(key=lambda x: x[0])
-print(vals)
 
-
-c = 0 
-for i in range(len(vals) - 1):
-    curr = vals[i]
-    nxt = vals[i + 1]
-
-    if curr[1] > nxt[0]:
-        print("ye")
-        vals[i+1][0] = curr[0]
-        if curr[1] > nxt[1]:
-            vals[i+1][1] = curr[1]
+windows = []
+for val in vals:
+    if not windows:
+        windows.append(val)
     else:
-        print("no", curr[0], curr[1])
-        c += abs(curr[1] - curr[0]) + 1
+        overlap = False
+        for win in windows:
+            if val[0] >= win[0] and val[0] <= win[1]:
+                overlap = True
+                if val[1] >= win[1]:
+                    win[1] = val[1]
+                break
+        if not overlap:
+            windows.append(val)
 
-c += abs(vals[-1][1] - vals[-1][0]) + 1
+c = 0
+for win in windows:
+    c += abs(win[1] - win[0]) + 1
 
-print(vals)
 print(c)
